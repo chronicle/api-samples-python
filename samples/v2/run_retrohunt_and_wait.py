a752b80f6f09c293ceeec1c5190ff5ca455d8401
Chronicle Team <copybara-servicebot@google.com>
2021-02-10 15:18:59 -0800

    Internal change
    PiperOrigin-RevId: 356838194

---
+++ samples/v2/run_retrohunt_and_wait.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Executable and reusable sample for running a retrohunt and waiting for it.
+
+This module demonstrates combining multiple single-purpose modules into a larger
+workflow.
+"""
+
+import argparse
+import datetime
+import pprint
+import time
+from typing import Any, Mapping, Sequence, Tuple
+
+from google.auth.transport import requests
+
+from google3.third_party.chronicle.detection_api.samples.v2 import cancel_retrohunt
+from google3.third_party.chronicle.detection_api.samples.v2 import chronicle_auth
+from google3.third_party.chronicle.detection_api.samples.v2 import datetime_converter
+from google3.third_party.chronicle.detection_api.samples.v2 import get_retrohunt
+from google3.third_party.chronicle.detection_api.samples.v2 import list_detections
+from google3.third_party.chronicle.detection_api.samples.v2 import run_retrohunt
+
+from . import cancel_retrohunt
+from . import chronicle_auth
+from . import get_retrohunt
+from . import list_detections
+from . import run_retrohunt
+
+CHRONICLE_API_BASE_URL = "https://backstory.googleapis.com"
+
+# Sleep duration used to wait until next retrohunt status check.
+DEFAULT_SLEEP_SECONDS = 5
+# Timeout used to wait until retrohunt is complete.
+DEFAULT_TIMEOUT_MINUTES = 1440.0  # 1 day = 60 * 24 = 1440 minutes.
+
+
+def get_retrohunt_info(
+    retrohunt: Mapping[str, Any]) -> Tuple[str, str, str, float]:
+  """Helper function to extract versionId, retrohuntId, state, and progressPercentage from retrohunt.
+
+  Args:
+    retrohunt: Retrohunt in a Mapping format.
+  Returns:
+    versionId, retrohuntId, state, progressPercentage.
+  """
+  return (retrohunt.get("versionId", ""), retrohunt.get("retrohuntId", ""),
+          retrohunt.get("state", "STATE_UNSPECIFIED"),
+          retrohunt.get("progressPercentage", 0.0))
+
+
+def run_retrohunt_and_wait(
+    http_session: requests.AuthorizedSession,
+    version_id: str,
+    start_time: datetime.datetime,
+    end_time: datetime.datetime,
+    sleep_seconds: int = DEFAULT_SLEEP_SECONDS,
+    timeout_minutes: float = DEFAULT_TIMEOUT_MINUTES,
+    page_size: int = 0
+    ) -> Tuple[Sequence[Mapping[str, Any]], str]:
+  """Runs a retrohunt and wait, and receive detections.
+
+  When retrohunt does not complete within the 'timeout_minutes' time period,
+  it cancels the retrohunt and returns TimeoutError.
+
+  Args:
+    http_session: Authorized session for HTTP requests.
+    version_id: Unique ID of the detection rule to retrieve errors for
+      ("ru_<UUID>" or "ru_<UUID>@v_<seconds>_<nanoseconds>"). If a version
+      suffix isn't specified we use the rule's latest version.
+    start_time: The start time of the time range the retrohunt will process.
+    end_time: The end time of the time range the retrohunt will process.
+    sleep_seconds: Optional interval between retrohunt status checks, until it's
+      DONE or CANCELLED.
+    timeout_minutes: Optional timeout in minutes. This is used to wait for the
+      retrohunt to complete.
+    page_size: Maximum number of detections in the response. Must be
+      non-negative. This is optional. If not provided, default value 100 is
+      applied.
+
+  Returns:
+    First page of detections and page token, which is a Base64 token for
+    getting the detections of the next page (an empty token string means the
+    currently retrieved page is the last one).
+
+  Raises:
+    requests.exceptions.HTTPError: HTTP request resulted in an error
+      (response.status_code >= 400).
+    TimeoutError: When retrohunt does not complete by timeout.
+  """
+
+  deadline = datetime.datetime.now() + datetime.timedelta(
+      minutes=timeout_minutes)
+  # Start RunRetrohunt by calling RunRetrohunt.
+  retrohunt_rep = run_retrohunt.run_retrohunt(http_session, version_id,
+                                              start_time, end_time)
+  version_id, retrohunt_id, state, progress_percentage = get_retrohunt_info(
+      retrohunt_rep)
+  print(f"Retrohunt started. retrohunt_id: {retrohunt_id}")
+  now = datetime.datetime.now()
+  while now < deadline and state not in ("DONE", "CANCELLED"):
+    print((f"Waiting for retrohunt to complete. Retrohunt is running at "
+           f"{progress_percentage}% .."))
+    time.sleep(sleep_seconds)
+    retrohunt_rep = get_retrohunt.get_retrohunt(http_session,
+                                                version_id,
+                                                retrohunt_id)
+    version_id, retrohunt_id, state, progress_percentage = get_retrohunt_info(
+        retrohunt_rep)
+    now = datetime.datetime.now()
+
+  # We finished waiting for the retrohunt to complete. We cancel the retrohunt
+  # if it is still running.
+  if state == "RUNNING":
+    print((f"Retrohunt did not complete. "
+           f"Cancelling retrohunt for versionID: {version_id}"))
+    # When cancel_retrohunt fails, it raises error and stop the script here.
+    cancel_retrohunt.cancel_retrohunt(http_session, version_id,
+                                      retrohunt_id)
+    raise TimeoutError(
+        f"Retrohunt not completed after {timeout_minutes} minutes.")
+
+  print("Returning first page of detections.")
+  return list_detections.list_detections(
+      http_session,
+      version_id=version_id,
+      page_size=page_size,
+      detection_start_time=start_time,
+      detection_end_time=end_time)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  chronicle_auth.add_argument_credentials_file(parser)
+  parser.add_argument(
+      "-vi",
+      "--version_id",
+      type=str,
+      required=True,
+      help="version ID ('ru_<UUID>[@v_<seconds>_<nanoseconds>]')")
+  parser.add_argument(
+      "-st",
+      "--start_time",
+      type=datetime_converter.iso8601_datetime_utc,
+      required=True,
+      help="Retrohunt start time in UTC ('yyyy-mm-ddThh:mm:ssZ')")
+  parser.add_argument(
+      "-et",
+      "--end_time",
+      type=datetime_converter.iso8601_datetime_utc,
+      required=True,
+      help="Retrohunt end time in UTC ('yyyy-mm-ddThh:mm:ssZ')")
+  parser.add_argument(
+      "-ss",
+      "--sleep_seconds",
+      type=int,
+      default=DEFAULT_SLEEP_SECONDS,
+      help="interval between retrohunt status polls in seconds (default = 5)")
+  parser.add_argument(
+      "-tm",
+      "--timeout_minutes",
+      type=float,
+      default=DEFAULT_TIMEOUT_MINUTES,
+      help=("timeout in minutes (default = 1 day) used to wait for retrohunt"
+            "to complete and return detections"))
+  parser.add_argument(
+      "-s",
+      "--page_size",
+      type=int,
+      required=False,
+      help="maximum number of detections to return")
+  args = parser.parse_args()
+  session = chronicle_auth.init_session(
+      chronicle_auth.init_credentials(args.credentials_file))
+  detections, next_page_token = run_retrohunt_and_wait(
+      session, args.version_id, args.start_time, args.end_time,
+      args.sleep_seconds, args.timeout_minutes, args.page_size)
+
+  pprint.pprint(detections)
+  print(f"Next page token: {next_page_token}")
---
+++ tests/v2/run_retrohunt_and_wait_test.py
@@ -0,0 +1,224 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Unit tests for the "run_retrohunt_and_wait" module."""
+import datetime
+import unittest
+from unittest import mock
+
+from google.auth.transport import requests
+from samples.v2 import run_retrohunt_and_wait
+
+
+class RunRetrohuntAndWaitTest(unittest.TestCase):
+
+  @mock.patch("time.sleep", return_value=None)
+  @mock.patch.object(requests, "AuthorizedSession", autospec=True)
+  @mock.patch.object(requests.requests, "Response", autospec=True)
+  def test_happy_path(self, mock_response, mock_session, mock_sleep):
+    # run retrohunt, wait retrohunt, list detections, all success.
+    type(mock_response).status_code = mock.PropertyMock(
+        side_effect=[200, 200, 200])
+    mock_response.raise_for_status.side_effect = [None, None, None]
+    rule_id = "ru_bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
+    version_id = f"{rule_id}@v_123456789_12345789"
+    # Response for RunRetrohunt
+    running_rh = {
+        "retrohuntId":
+            "oh_aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+        "ruleId": rule_id,
+        "versionId": version_id,
+        "eventStartTime":
+            "2021-01-01T00:00:00Z",
+        "eventEndTime":
+            "2021-01-02T00:00:00Z",
+        "retrohuntStartTime":
+            "2020-01-01T00:00:00Z",
+        "retrohuntEndTime":
+            "2020-01-02T00:00:00Z",
+        "state":
+            "RUNNING",
+        "progressPercentage":
+            "0.0",
+    }
+    # Response for GetRetrohunt
+    completed_rh = running_rh.copy()
+    completed_rh["state"] = "DONE"
+    completed_rh["progressPercentage"] = "100.0"
+
+    # Response for ListDetections
+    expected_detection = {
+        "id": "de_12345678-1234-1234-1234-1234567890ab",
+        "type": "RULE_DETECTION",
+        "createdTime": "2020-11-05T12:00:00Z",
+        "detectionTime": "2020-11-05T01:00:00Z",
+        "timeWindow": {
+            "startTime": "2020-11-05T00:00:00Z",
+            "endTime": "2020-11-05T01:00:00Z",
+        },
+        "detection": [
+            {
+                "ruleId": "ru_bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb",
+                "ruleName": "rule content",
+                "ruleVersion": version_id,
+                "urlBackToProduct": "https://chronicle.security",
+                "alertState": "ALERTING",
+                "ruleType": "MULTI_EVENT",
+                "detectionFields": [
+                    {
+                        "key": "fieldName",
+                        "value": "fieldValue",
+                    }
+                ]
+            }
+        ],
+    }
+    expected_page_token = "page token"
+    mock_response.json.side_effect = [
+        running_rh, completed_rh, {
+            "detections": [expected_detection],
+            "nextPageToken": expected_page_token,
+        }
+    ]
+    mock_session.request.return_value = mock_response
+
+    end_time = datetime.datetime.now()
+    start_time = end_time - datetime.timedelta(hours=1)
+
+    got_detections, next_page_token = wait.run_retrohunt_and_wait(
+        mock_session, rule_id, start_time, end_time)
+    self.assertEqual(len(got_detections), 1)
+    self.assertEqual(got_detections[0], expected_detection)
+    self.assertEqual(next_page_token, expected_page_token)
+    # Sleep should be called only once.
+    self.assertEqual(mock_sleep.call_count, 1)
+
+  @mock.patch.object(requests, "AuthorizedSession", autospec=True)
+  @mock.patch.object(requests.requests, "Response", autospec=True)
+  def test_retrohunt_not_complete(self, mock_response, mock_session):
+    mock_session.request.return_value = mock_response
+    type(mock_response).status_code = mock.PropertyMock(
+        side_effect=[200, 200, 200, 200])
+    rule_id = "ru_bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
+    version_id = f"{rule_id}@v_123456789_12345789"
+    # Response for RunRetrohunt.
+    running_rh = {
+        "retrohuntId":
+            "oh_aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+        "ruleId": rule_id,
+        "versionId": version_id,
+        "eventStartTime":
+            "2021-01-01T00:00:00Z",
+        "eventEndTime":
+            "2021-01-02T00:00:00Z",
+        "retrohuntStartTime":
+            "2020-01-01T00:00:00Z",
+        "retrohuntEndTime":
+            "2020-01-02T00:00:00Z",
+        "state":
+            "RUNNING",
+        "progressPercentage":
+            "0.0",
+    }
+
+    # We'll call run_retrohunt_and_wait with sleep_secounds=2,
+    # timeout_minutes 0.05(=3sec). With this setup, we make 2 GetRetrohunt calls
+    # and exit the loop.
+    # Flow in the loop will be: sleep 2sec -> call GetRetrohunt ->
+    #   about 2.5 secs has passed -> sleep 2sec -> call GetRetrohunt
+
+    # Responses for the two GetRetrohunt calls.
+    running_rh2 = running_rh.copy()
+    running_rh2["progressPercentage"] = "10.0"
+    running_rh3 = running_rh.copy()
+    running_rh3["progressPercentage"] = "20.0"
+
+    # Order of reponses
+    # 1. RunRetrohunt
+    # 2. 1st call for GetRetrohunt
+    # 3. 2nd call for GetRetrohunt
+    # 4. Empty response for CancelRetrohunt
+    mock_response.json.side_effect = [
+        running_rh, running_rh2, running_rh3, None
+    ]
+    mock_response.raise_for_status.side_effect = ([None, None, None, None])
+
+    end_time = datetime.datetime.now()
+    start_time = end_time - datetime.timedelta(hours=1)
+
+    with self.assertRaises(TimeoutError):
+      wait.run_retrohunt_and_wait(
+          mock_session,
+          rule_id,
+          start_time,
+          end_time,
+          sleep_seconds=2,
+          timeout_minutes=0.05)
+
+  @mock.patch.object(requests, "AuthorizedSession", autospec=True)
+  @mock.patch.object(requests.requests, "Response", autospec=True)
+  def test_wait_retrohunt_timeout_cancel_retrohunt_error(
+      self, mock_response, mock_session):
+    mock_session.request.return_value = mock_response
+    type(mock_response).status_code = mock.PropertyMock(
+        side_effect=[200, 200, 400])
+    rule_id = "ru_bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"
+    version_id = f"{rule_id}@v_123456789_12345789"
+    # Response for RunRetrohunt.
+    running_rh = {
+        "retrohuntId":
+            "oh_aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+        "ruleId": rule_id,
+        "versionId": version_id,
+        "eventStartTime":
+            "2021-01-01T00:00:00Z",
+        "eventEndTime":
+            "2021-01-02T00:00:00Z",
+        "retrohuntStartTime":
+            "2020-01-01T00:00:00Z",
+        "retrohuntEndTime":
+            "2020-01-02T00:00:00Z",
+        "state":
+            "RUNNING",
+        "progressPercentage":
+            "0.0",
+    }
+    # Response for GetRetrohunt.
+    running_rh2 = running_rh.copy()
+    running_rh2["progressPercentage"] = "10.0"
+    # We'll call run_retrohunt_and_wait with sleep_seconds=2,
+    # timeout_minutes=0.02(=1.2sec), so we make only 1 GetRetrohunt call.
+    # Call flow will be RunRetrohunt, GetRetrohunt, CancelRetrohunt.
+    mock_response.json.side_effect = [running_rh, running_rh2, None]
+    # CancelRetrohunt will fail.
+    mock_response.raise_for_status.side_effect = (
+        [None,
+         None,
+         requests.requests.exceptions.HTTPError()])
+
+    end_time = datetime.datetime.now()
+    start_time = end_time - datetime.timedelta(hours=1)
+
+    with self.assertRaises(requests.requests.exceptions.HTTPError):
+      wait.run_retrohunt_and_wait(
+          mock_session,
+          rule_id,
+          start_time,
+          end_time,
+          sleep_seconds=2,
+          timeout_minutes=0.02)
+
+
+if __name__ == "__main__":
+  unittest.main()
