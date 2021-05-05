7c0597dedf0c726a1dbf98513d6e86e6f1562a31
Jason Wong <wojason@google.com>
2021-05-05 13:14:46 -0700

    Add sample for updating a list (which overrides the list content)
    PiperOrigin-RevId: 372192711

---
+++ lists/update_list.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Executable and reusable sample for updating a list."""
+
+import argparse
+from typing import Optional, Sequence
+
+from google.auth.transport import requests
+
+from common import chronicle_auth
+
+CHRONICLE_API_BASE_URL = "https://backstory.googleapis.com"
+
+
+def update_list(http_session: requests.AuthorizedSession, name: str,
+                description: Optional[str],
+                content_lines: Sequence[str]) -> str:
+  """Updates a list.
+
+  Args:
+    http_session: Authorized session for HTTP requests.
+    name: Name of existing list.
+    description: Optional new description of the list.
+    content_lines: Array containing each line of the list's content.
+
+  Returns:
+    Timestamp of when the updated list was written.
+
+  Raises:
+    requests.exceptions.HTTPError: HTTP request resulted in an error
+      (response.status_code >= 400).
+  """
+  url = f"{CHRONICLE_API_BASE_URL}/v2/lists"
+  body = {
+      "name": name,
+      "lines": content_lines,
+  }
+  update_fields = ["list.lines"]
+
+  if description:
+    update_fields.append("list.description")
+    body["description"] = description
+
+  params = {"update_mask": ",".join(update_fields)}
+
+  response = http_session.request("PATCH", url, params=params, json=body)
+  # Expected server response:
+  # {
+  #   "name": "<list name>",
+  #   "description": "<list description>",
+  #   "createTime": "yyyy-mm-ddThh:mm:ss.ssssssZ",
+  #   "lines": [
+  #     "<line 1>",
+  #     "<line 2>",
+  #     ...
+  #   ]
+  # }
+
+  if response.status_code >= 400:
+    print(response.text)
+  response.raise_for_status()
+  return response.json()["createTime"]
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  chronicle_auth.add_argument_credentials_file(parser)
+  parser.add_argument(
+      "-n", "--name", type=str, required=True, help="unique name for the list")
+  parser.add_argument(
+      "-d",
+      "--description",
+      type=str,
+      help="description of the list. Omit this to use previous description."
+  )
+  parser.add_argument(
+      "-f",
+      "--list_file",
+      type=argparse.FileType("r"),
+      required=True,
+      # File example:
+      #   python3 -m lists.update_list <other args> -f <path>
+      # STDIN example:
+      #   cat <path> | python3 -m lists.update_list <other args> -f -
+      help="path of a file containing the list content, or - for STDIN")
+  args = parser.parse_args()
+  session = chronicle_auth.init_session(
+      chronicle_auth.init_credentials(args.credentials_file))
+
+  t = update_list(session, args.name, args.description,
+                  args.list_file.read().splitlines())
+  print(f"List updated successfully, at time {t}")
---
+++ lists/update_list_test.py
@@ -0,0 +1,59 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Unit tests for the "update_list" module."""
+
+import unittest
+from unittest import mock
+
+from google.auth.transport import requests
+from . import update_list
+
+
+class UpdateListTest(unittest.TestCase):
+
+  @mock.patch.object(requests, "AuthorizedSession", autospec=True)
+  @mock.patch.object(requests.requests, "Response", autospec=True)
+  def test_http_error(self, mock_response, mock_session):
+    mock_session.request.return_value = mock_response
+    type(mock_response).status_code = mock.PropertyMock(return_value=400)
+    mock_response.raise_for_status.side_effect = (
+        requests.requests.exceptions.HTTPError())
+
+    with self.assertRaises(requests.requests.exceptions.HTTPError):
+      update_list.update_list(mock_session, "name", "description",
+                              ["content line"])
+
+  @mock.patch.object(requests, "AuthorizedSession", autospec=True)
+  @mock.patch.object(requests.requests, "Response", autospec=True)
+  def test_happy_path(self, mock_response, mock_session):
+    mock_session.request.return_value = mock_response
+    type(mock_response).status_code = mock.PropertyMock(return_value=200)
+    expected_time = "2021-01-01T00:00:00Z"
+    expected_list = {
+        "name": "name",
+        "description": "description",
+        "lines": ["content line 1", "content line 2"],
+        "createTime": expected_time
+    }
+    mock_response.json.return_value = expected_list
+
+    actual_time = update_list.update_list(
+        mock_session, "name", "description",
+        ["content line 1", "content line 2"])
+    self.assertEqual(actual_time, expected_time)
+
+
+if __name__ == "__main__":
+  unittest.main()
