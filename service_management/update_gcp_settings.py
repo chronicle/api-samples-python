74da743e8c0470b4b7d54583bf97e865cb7ed3ec
Chronicle Team <copybara-servicebot@google.com>
2021-11-01 12:21:45 -0700

    Add sample code for UpdateGCPSettings.
    PiperOrigin-RevId: 406888994

---
+++ service_management/update_gcp_settings.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Executable and reusable sample for updating GCP settings.
+
+In Chronicle, customers can associate their GCP organizations with their
+Chronicle instances. After that, customers can control how their logs flow
+into Chronicle by GCP settings. This example provides a programmatic way to
+update such settings.
+"""
+
+import argparse
+import sys
+from typing import Optional, Sequence
+
+from google.auth.transport import requests
+
+from common import chronicle_auth
+
+SERVICE_MANAGEMENT_API_BASE_URL = "https://chronicleservicemanager.googleapis.com"
+
+AUTHORIZATION_SCOPES = ["https://www.googleapis.com/auth/cloud-platform"]
+
+
+def initialize_command_line_args(
+    args: Optional[Sequence[str]] = None) -> Optional[argparse.Namespace]:
+  """Initializes and checks all the command-line arguments."""
+  parser = argparse.ArgumentParser()
+  chronicle_auth.add_argument_credentials_file(parser)
+  parser.add_argument(
+      "--organization_id",
+      type=int,
+      required=True,
+      help="the GCP organization ID for the settings")
+  parser.add_argument("--enable", dest="ingestion", action="store_true")
+  parser.add_argument("--disable", dest="ingestion", action="store_false")
+  parser.set_defaults(ingestion=True)
+
+  # Sanity checks for the command-line arguments.
+  parsed_args = parser.parse_args(args)
+  if parsed_args.organization_id >= 2**64 or parsed_args.organization_id < 0:
+    print("Error: organization ID should not be bigger than 2^64")
+    return None
+
+  return parsed_args
+
+
+def update_gcp_settings(http_session: requests.AuthorizedSession,
+                        organization_id: int, enable_ingestion: bool) -> None:
+  """Updates GCP settings with Chronicle.
+
+  Args:
+    http_session: Authorized session for HTTP requests.
+    organization_id: GCP organization ID for the settings.
+    enable_ingestion: Whether to enable GCP log ingestion or not.
+
+  Raises:
+    requests.exceptions.HTTPError: HTTP request resulted in an error
+      (response.status_code >= 400).
+  """
+  name = f"organizations/{organization_id}/gcpAssociations/{organization_id}/gcpSettings"
+  url = f"{SERVICE_MANAGEMENT_API_BASE_URL}/v1/{name}"
+
+  body = {
+      "log_flow": "LOG_FLOW_INACTIVE",
+  }
+  if enable_ingestion:
+    body["log_flow"] = "LOG_FLOW_ACTIVE"
+
+  response = http_session.request("PATCH", url, json=body)
+
+  if response.status_code >= 400:
+    print(response.text)
+  response.raise_for_status()
+
+
+if __name__ == "__main__":
+  cli = initialize_command_line_args()
+  if not cli:
+    sys.exit(1)  # A sanity check failed.
+
+  session = chronicle_auth.initialize_http_session(
+      cli.credentials_file, scopes=AUTHORIZATION_SCOPES)
+  update_gcp_settings(session, cli.organization_id, cli.ingestion)
