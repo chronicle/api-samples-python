9285beea618481b430c1317a3cb64d8099e1e4cb
Chronicle Team <copybara-servicebot@google.com>
2022-07-01 08:08:42 -0700

    Internal change
    PiperOrigin-RevId: 458471961

---
+++ feeds/disable_feed.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Executable and reusable sample for disabling a feed."""
+
+import argparse
+
+from google.auth.transport import requests
+
+from common import chronicle_auth
+from common import regions
+
+CHRONICLE_API_BASE_URL = "https://backstory.googleapis.com"
+
+
+def disable_feed(http_session: requests.AuthorizedSession, name: str):
+  """Disable a specific feed.
+
+  Args:
+    http_session: Authorized session for HTTP requests.
+    name: Unique name for the feed.
+
+  Raises:
+    requests.exceptions.HTTPError: HTTP request resulted in an error
+      (response.status_code >= 400).
+  """
+  url = f"{CHRONICLE_API_BASE_URL}/v1/feeds/{name}:disable"
+
+  response = http_session.request("POST", url)
+  # Expected server response:
+  # {}
+
+  if response.status_code >= 400:
+    print(response.text)
+  response.raise_for_status()
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  chronicle_auth.add_argument_credentials_file(parser)
+  regions.add_argument_region(parser)
+  parser.add_argument(
+      "-n", "--name", type=str, required=True, help="unique name for the feed")
+
+  args = parser.parse_args()
+  CHRONICLE_API_BASE_URL = regions.url(CHRONICLE_API_BASE_URL, args.region)
+  session = chronicle_auth.initialize_http_session(args.credentials_file)
+  disable_feed(session, args.name)
